938436930acab5c08ec5b63e7bcdbcf944321638
Enzo Martino <luigi.m.vincenzo@avanade.com>
2019-03-17 00:17:36 +0100

    Updated gitignore

--- JustFit/src/models/common/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDt9YTgEaCxhPpya_S8zUJLeiHdvG1Y27c",
-    authDomain: "justfit-bdde4.firebaseapp.com",
-    databaseURL: "https://justfit-bdde4.firebaseio.com",
-    projectId: "justfit-bdde4",
-    storageBucket: "",
-    messagingSenderId: "328634205501"
-  };
