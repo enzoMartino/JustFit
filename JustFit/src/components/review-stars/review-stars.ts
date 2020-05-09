3841fc73507eaa7341d908a2f7625b8c611ed1da
Vincenzo Luigi Martino <vincenzo.l.martino@avanade.com>
2020-05-09 23:54:37 +0200

    eliminato import inutilizzato

--- JustFit/src/components/review-stars/review-stars.ts
+++ JustFit/src/components/review-stars/review-stars.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'review-stars',
